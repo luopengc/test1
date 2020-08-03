2ea8f66f71fedb4d1c4769f5b94b614dfc9fd18f
再见理想 <68989005+luopengc@users.noreply.github.com>
2020-08-03 20:23:26 +0800

    Update main.java

--- src/com/test/main.java
+++ src/com/test/main.java
@@ -3,6 +3,6 @@ package com.test;
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("3io4io9sdkjfsodfjikwoerfrjk");
+        System.out.println("1111111111111111111111");
     }
 }
